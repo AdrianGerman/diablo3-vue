fc3df56c987aad212283eecfdc3b63ac902503f2
AdrianGerman <adrian.german1019@gmail.com>
2024-05-22 10:11:10 -0600

    folder api

---
+++ src/api/oauthexampple.js
@@ -0,0 +1,23 @@
+// Traer el método post de 'axios'
+import { post } from 'axios';
+
+
+// Aqui deberas poner tu información del cliente, ya que es privada este solo es un archivo de ejemplo.
+const clientId = 'clientId';
+const clientSecret = 'clientSecret';
+
+// API URL para hacer oauth
+const region = 'eu';
+const API_URL = `https://${region}.battle.net/oauth/token`;
+
+function getToken() {
+    const body = new FormData();
+    body.append('grant_type', 'client_credentials');
+    const config = {
+        headers: { 'Content-Type': 'multipart/form-data' },
+        auth: { username: clientId, password: clientSecret }
+    }
+    return post(API_URL, body, config);
+}
+
+export { getToken };
