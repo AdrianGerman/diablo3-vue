1f8979323302513353a99a75a844a03f74fdaf15
AdrianGerman <adrian.german1019@gmail.com>
2024-05-22 09:45:42 -0600

    installing libraries and config bootstrap

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <br>
+    <b-button>Just a Button!</b-button>
   </div>
 </template>
 
