d634b953314ec0b0d05c338a83e5093b9f0862fe
AdrianGerman <adrian.german1019@gmail.com>
2024-05-23 11:38:56 -0600

    navbar and footer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="container">
+        <HeaderBar />
         <router-view />
+        <FooterBar />
     </div>
 </template>
 
 <script>
+import HeaderBar from '@/components/HeaderBar'
+import FooterBar from '@/components/Footer/FooterBar'
 export default {
-    name: 'MainLayout'
+    name: 'MainLayout',
+    components: {
+        HeaderBar,
+        FooterBar
+    }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,28 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from '@fortawesome/free-solid-svg-icons'
+import { faVuejs, faBootstrap, faFontAwesome, faGithub, faBattleNet } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Le añadimos los iconos que acabamos de importar (todos, los de tipo solid y de tipo brand)
+library.add(
+  faSkull,
+  faCrown,
+  faDungeon,
+  faHatWizard,
+  faHammer,
+  faGem,
+  faVuejs,
+  faBootstrap,
+  faFontAwesome,
+  faGithub,
+  faBattleNet
+)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
