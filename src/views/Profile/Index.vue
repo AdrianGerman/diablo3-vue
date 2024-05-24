155e3b1379806f433d84183280fa73d6c704982f
AdrianGerman <adrian.german1019@gmail.com>
2024-05-24 14:49:20 -0600

    view profile

--- src/views/Profile/Index.vue
+++ src/views/Profile/Index.vue
@@ -1,11 +1,54 @@
 <template>
     <div>
+        <BaseLoading v-if="isLoading" />
         <h1>Profile view</h1>
     </div>
 </template>
 
 <script>
+import { getApiAccount } from '@/api/search';
+import setError from '@/mixins/setError';
+import BaseLoading from '@/components/BaseLoading';
 export default {
-    name: 'ProfileView'
+    name: 'ProfileView',
+    mixins: [
+        setError
+    ],
+    components: { BaseLoading },
+    data() {
+        return {
+            isLoading: false,
+            profileData: null
+        }
+    },
+    created() {
+        this.isLoading = true
+        const { region, battleTag: account } = this.$route.params
+        this.fetchData(region, account)
+    },
+    methods: {
+        fetchData(region, account) {
+            getApiAccount({ region, account })
+                .then(({ data }) => {
+                    this.profileData = data
+                })
+                .catch((err) => {
+                    this.profileData = null
+                    const errObj = {
+                        routeParams: this.$route.params,
+                        message: err.message
+                    }
+                    if (err.response) {
+                        errObj.data = err.response.data
+                        errObj.status = err.response.status
+                    }
+                    this.setApiErr(errObj)
+                    this.$router.push({ name: 'Error' })
+                })
+                .finally(() => {
+                    this.isLoading = false
+                })
+        }
+    }
 }
 </script>
