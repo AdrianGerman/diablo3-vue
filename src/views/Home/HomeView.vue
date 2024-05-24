5fb8c1437bc3cfa4de9081ab0b261f84f8cf049a
AdrianGerman <adrian.german1019@gmail.com>
2024-05-24 08:46:04 -0600

    home page

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="home">
-    <h1>Home page</h1>
+    <HomeTitle />
+    <MainForm />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import HomeTitle from './HomeTitle';
+import MainForm from './HomeForm';
 export default {
   name: 'Home',
   components: {
+    HomeTitle, MainForm
   }
 }
 </script>
