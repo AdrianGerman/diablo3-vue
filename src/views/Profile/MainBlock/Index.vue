20ee24d32f8c07d7ef68b8b478909dd1a74f5329
AdrianGerman <adrian.german1019@gmail.com>
2024-05-25 11:01:43 -0600

    heroes list

--- src/views/Profile/MainBlock/Index.vue
+++ src/views/Profile/MainBlock/Index.vue
@@ -2,6 +2,8 @@
   <div class="grid-container">
     <div class="grid-item item-left">
       <TopHeroes v-if="hasHeroes" :heroes="topHeroes" />
+
+      <HeroesList v-if="hasHeroesList" :heroes="heroesList" />
     </div>
     <div class="grid-item grid-right">
       <h1>Derecha</h1>
@@ -11,9 +13,10 @@
 
 <script>
 import TopHeroes from './TopHeroes/Index'
+import HeroesList from './HeroesList/Index'
 export default {
   name: 'MainBlock',
-  components: { TopHeroes },
+  components: { TopHeroes, HeroesList },
   props: {
     profileData: {
       type: Object,
@@ -26,6 +29,12 @@ export default {
     },
     topHeroes() {
       return this.profileData.heroes.slice(0, 3)
+    },
+    hasHeroesList() {
+      return this.profileData.heroes.length > 3
+    },
+    heroesList() {
+      return this.profileData.heroes.slice(3, this.profileData.heroes.length)
     }
   }
 }
