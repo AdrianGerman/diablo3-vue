7ff07d05c8d68371b1b6b6aed71b6a82482aed05
AdrianGerman <adrian.german1019@gmail.com>
2024-05-28 09:59:12 -0600

    asynchronous components

--- src/views/Hero/HeroSkills/Index.vue
+++ src/views/Hero/HeroSkills/Index.vue
@@ -3,27 +3,50 @@
         <h2 class="font-diablo">Skills</h2>
         <hr class="bg-white">
 
-        <ActiveSkills :skills="skills.active" />
-        <hr>
-        <PassiveSkills :skills="skills.passive" />
+        <b-nav pills small>
+            <b-nav-item :active="!isPassiveSkillsActive" @click="changeComponent('ActiveSkills')">Active</b-nav-item>
+            <b-nav-item :active="isPassiveSkillsActive" @click="changeComponent('PassiveSkills')">Passive</b-nav-item>
+        </b-nav>
+
+        <component :is="activeComponent" :skills="componentProps" />
 
     </div>
 </template>
 
 <script>
-import ActiveSkills from './ActiveSkills'
-import PassiveSkills from './PassiveSkills'
-
 export default {
     name: 'HeroSkills',
     components: {
-        ActiveSkills,
-        PassiveSkills
+        // Componentes Dinámicos
+        ActiveSkills: () => import(/* webpackChunkName: "ActiveSkills" */ './ActiveSkills'),
+        PassiveSkills: () => import(/* webpackChunkName: "PassiveSkills" */ './PassiveSkills')
     },
     props: {
         skills: {
             required: true,
             type: Object
+        }
+    },
+    data() {
+        return {
+            activeComponent: 'ActiveSkills'
+        }
+    },
+    computed: {
+        /**
+         * Props dinámicos para componentes dinámicos
+         * @returns {Object}
+         */
+        componentProps() {
+            return this.activeComponent === 'ActiveSkills' ? this.skills.active : this.skills.passive;
+        },
+        isPassiveSkillsActive() {
+            return this.activeComponent === 'PassiveSkills';
+        }
+    },
+    methods: {
+        changeComponent(component) {
+            this.activeComponent = component;
         }
     }
 }
